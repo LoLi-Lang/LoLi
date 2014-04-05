948cfc0deefed9545d9f6e4b95d5b17fad24d0da
Z-Shang <shangzhanlin@gmail.com>
2014-04-05 14:57:36 -0500

    add clang test

--- src/build_test.sh
+++ src/build_test.sh
@@ -1,1 +1,5 @@
-g++ -std=c++11 -o test loli_symbols.cpp loli_types.cpp  loli_cons.cpp loli_main.cpp && ./test
+echo "G++: "
+g++ -std=c++11 -o gcctest loli_symbols.cpp loli_types.cpp  loli_cons.cpp loli_main.cpp && ./gcctest
+
+echo "CLANG++: "
+clang++ -std=c++11 -o clangtest loli_symbols.cpp loli_types.cpp  loli_cons.cpp loli_main.cpp && ./clangtest
