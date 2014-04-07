1ffe0dbb4f88edaf3c6262b88ec85a20121a64d8
Z-Shang <shangzhanlin@gmail.com>
2014-04-07 09:54:31 -0500

    add Makefile

--- src/build_test.sh
+++
@@ -1,5 +0,0 @@
-echo "G++: "
-g++ -std=c++11 -o gcctest loli_prim.cpp loli_symbols.cpp loli_types.cpp  loli_cons.cpp loli_main.cpp && ./gcctest
-
-echo "CLANG++: "
-clang++ -std=c++11 -o clangtest loli_prim.cpp loli_symbols.cpp loli_types.cpp  loli_cons.cpp loli_main.cpp && ./clangtest
