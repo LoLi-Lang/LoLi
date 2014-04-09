a3568aa6682f29801e4233d1bfa40a7f038e9898
Z-Shang <shangzhanlin@gmail.com>
2014-04-09 13:46:37 -0500

    clean up

--- src/loli_types.cpp
+++ src/loli_types.cpp
@@ -18,35 +18,31 @@
 
 #include <iostream>
 #include <string>
-//#include "loli.h"
+
 #include "loli_types.h"
 #include "loli_symbols.h"
 
 
 loliObj* mksym(std::string symName){
 	loliObj *tmp = new loliObj(SYM);
-	//tmp.type = SYM;
 	tmp->value = symName;
 	return tmp;
 }
 
 loliObj* mkint(int number){
 	loliObj *tmp = new loliObj(INT);
-	//tmp.type = INT;
 	tmp->value = std::to_string(number);
 	return tmp;
 }
 
 loliObj* mkflt(double number){
 	loliObj *tmp = new loliObj(FLT);
-	//tmp.type = FLT;
 	tmp->value = std::to_string(number);
 	return tmp;
 }
 
 loliObj* mkproc(loliObj::procedure &proc){
 	loliObj *tmp = new loliObj(PROC);
-	//tmp.type = PROC;
 	tmp->proc = proc;
 	return tmp;
 }
