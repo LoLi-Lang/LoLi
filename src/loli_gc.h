26f7e9882a45f6f389c4fa250bbd9ad29a047dd1
hcz <h.cz@qq.com>
2014-04-17 09:46:42 +0800

    add gc header

---
+++ src/loli_gc.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  loli_gc.h
+ *
+ *    Description:  GC of LoLi
+ *
+ *        Version:  1.0
+ *        Created:  04/17/2014 09:34:31 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  C.Hu (), h@itis.ga
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef __LOLI_GC_
+#define __LOLI_GC_
+
+//GC
+#include "gc/include/gc.h"
+#include "gc/include/gc_cpp.h"
+#include "gc/include/gc_allocator.h"
+
+#endif
