325a4d89abe9244a79b8fa1eea755c1fcd1a36d9
Z-Shang <shangzhanlin@gmail.com>
2014-04-18 04:43:48 -0500

    clean up

--- src/loli_gc.h
+++ src/loli_gc.h
@@ -3,16 +3,6 @@
  *
  *       Filename:  loli_gc.h
  *
-<<<<<<< HEAD
- *    Description:  GC Headers of LoLi (From HCZ@github)
- *
- *        Version:  1.0
- *        Created:  04/18/2014 04:04:30 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Z.Shang (), shangzhanlin@gmail.com
-=======
  *    Description:  GC of LoLi
  *
  *        Version:  1.0
@@ -21,23 +11,14 @@
  *       Compiler:  gcc
  *
  *         Author:  C.Hu (), h@itis.ga
->>>>>>> 26f7e9882a45f6f389c4fa250bbd9ad29a047dd1
  *   Organization:  
  *
  * =====================================================================================
  */
 
-<<<<<<< HEAD
-
 #ifndef __LOLI_GC_
 #define __LOLI_GC_
 
-=======
-#ifndef __LOLI_GC_
-#define __LOLI_GC_
-
-//GC
->>>>>>> 26f7e9882a45f6f389c4fa250bbd9ad29a047dd1
 #include "gc/include/gc.h"
 #include "gc/include/gc_cpp.h"
 #include "gc/include/gc_allocator.h"
