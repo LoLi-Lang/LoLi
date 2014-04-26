f31a751d3ae96d16136fb51aa38254d1b209a4ee
Z-Shang <shangzhanlin@gmail.com>
2014-04-25 23:26:25 -0500

    add blank read.h

---
+++ src/loli_read.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  loli_read.h
+ *
+ *    Description:  Reader of LoLi
+ *
+ *        Version:  1.0
+ *        Created:  04/25/2014 11:24:58 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Z.Shang (), shangzhanlin@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#ifndef __LOLI_READ_
+#define __LOLI_READ_
+
+
+#endif
