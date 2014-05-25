fbfdd38d860d8e07ea4d1eeb1b002272a2a05778
Z-Shang <shangzhanlin@gmail.com>
2014-05-25 09:38:32 +0800

    change the licence from gplv3 to gplv3+

--- src/loli_main.cpp
+++ src/loli_main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char * argv[]){
 		}
 	}
 //	guitest(argc, argv);
-	std::cout<<"LoLi PRPR!\nLoLi is a Free Software and you can do whatever you want with it under the licence GPLv3"<<std::endl;
+	std::cout<<"LoLi PRPR!\nLoLi is a Free Software and you can do whatever you want with it under the licence GPLv3 or any later version"<<std::endl;
 	while(true){
 		repl(top_env);
 	}
