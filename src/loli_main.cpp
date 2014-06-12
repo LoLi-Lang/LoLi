c1a45e4aed14300d3d3c20a4f650b00943813a5d
Z-Shang <shangzhanlin@gmail.com>
2014-06-12 15:56:02 +0800

    remove exit after read file

--- src/loli_main.cpp
+++ src/loli_main.cpp
@@ -107,7 +107,7 @@ int main(int argc, char * argv[]){
 				if(!readFile(argv[2], top_env)){
 					exit(0);
 				}
-				exit(0);
+				//exit(0);
 			}
 		}else if(strcmp(argv[1] , "--repl") == 0){
 			//DO NOTHING
