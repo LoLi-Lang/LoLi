916a231cc1bfaedcdfbdffe69599dd5ffade3351
hifever <fr.lu2010@gmail.com>
2014-06-26 23:18:44 +0800

    move the stack defination from loli_stack.cpp to loli_stack.h

---
+++ src/LoLi2/include/loli_stack.h
@@ -0,0 +1,62 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  loli_stack.cpp
+ *
+ *    Description:  Stack of LoLi
+ *
+ *        Version:  1.0
+ *        Created:  06/26/2014 06:31:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Z.Shang (), shangzhanlin@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef __LOLI_STACK_
+#define __LOLI_STACK_
+
+#include "loli_obj.h"
+
+#define STACK_SIZE 100
+
+struct loliStack{
+	loliObj stack[STACK_SIZE];
+	int obj_in_stack;
+
+	loliStack(){
+		obj_in_stack = 0;
+		for(int i = 0; i < STACK_SIZE; i++){
+			stack[i] = NULL;
+		}
+	}
+
+	bool isEmpty(){
+		return 0 == obj_in_stack;
+	}
+
+	bool isFull(){
+		return obj_in_stack == STACK_SIZE;
+	}
+
+	void push_obj(loliObj o){
+		if(this->isFull()){
+			loli_err("Stack Overflow");
+			return;
+		}
+		stack[++obj_in_stack] = o;
+	}
+	
+	loliObj pop_obj(){
+		if(this->isEmpty()){
+			loli_err("Stack Underflow");
+			return nil;
+		}
+		return stack[obj_in_stack--];
+	}
+}
+
+extern loliStack global_stack;
--- src/LoLi2/loli_stack.cpp
+++ src/LoLi2/loli_stack.cpp
@@ -19,37 +19,4 @@
 
 #include "include/loli_obj.h"
 
-#define STACK_SIZE 100
-
-struct loliStack{
-	loliObj stack[STACK_SIZE];
-	int obj_in_stack;
-
-	loliStack(){
-		obj_in_stack = 0;
-	}
-
-	bool isEmpty(){
-		return 0 == obj_in_stack;
-	}
-
-	bool isFull(){
-		return obj_in_stack == STACK_SIZE;
-	}
-
-	void push_obj(loliObj o){
-		if(this->isFull()){
-			loli_err("Stack Overflow");
-			return;
-		}
-		stack[++obj_in_stack] = o;
-	}
-	
-	loliObj pop_obj(){
-		if(this->isEmpty()){
-			loli_err("Stack Underflow");
-			return nil;
-		}
-		return stack[obj_in_stack--];
-	}
-}
+loliStack global_stack;
