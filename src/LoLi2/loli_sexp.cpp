c66849059e9d3d8e1b2f73e0e14b00e95c8c9de7
Z-Shang <shangzhanlin@gmail.com>
2014-07-15 16:11:10 +0800

    cons parser partly done (embed cons not done yet)

--- src/LoLi2/loli_sexp.cpp
+++ src/LoLi2/loli_sexp.cpp
@@ -25,7 +25,7 @@
 #include "../header/loli_gc.h"
 
 sexp* END = new (UseGC) sexp("END");
-sexp* SNIL = new (UseGC) sexp("NIL");
+sexp* SNIL = new (UseGC) sexp("SNIL");
 //Parser String -> SEXP
 
 sexp* parse_cons(std::string str);
@@ -65,21 +65,71 @@ sexp* to_sexp(std::string str){
 		}
 	}
 	if(str[0] == '('){
-		return parse_cons(str.substr(1, str.length() - 2));
+		sexp* tmp = new (UseGC) sexp("CONS");
+		tmp->next = parse_cons(str.substr(1, str.length() - 2));
+		return tmp;
 	}
 	//Return SNIL when get a blank input
-	return SNIL;
+	return END;
+}
+
+std::string pairUp(std::string exp){
+	int p = 0;
+	bool q = false;
+	for(ulong j = 0; j < exp.length(); j++){
+		if(exp[j] == '"'){
+			q = !q;
+		}
+		if(exp[j] == '('){
+			if(!q)
+				p = p + 1;
+		}else if(exp[j] == ')'){
+			if(!q)
+				p = p - 1;
+		}
+		if(p == 0 && !q){
+			return exp.substr(0, j + 1);
+		}
+	}
+	return exp;
 }
 
 sexp* parse_cons(std::string str){
 	while(isspace(str[0])){
 		str = str.substr(1);
 	}
+	std::cout<<"Parse CONS: " <<str<<std::endl;
 	if(str[0] == '\0'){
-		return SNIL;
+		sexp* tmp;
+		tmp = SNIL;
+		tmp->next = END;
+		return tmp;
 	}
-	sexp* tmp = new (UseGC) sexp("CONS");
 	//TODO:
 	//Finish the parser
-	return END;
+	for(ulong i = 0; i < str.length(); i++){
+		if(isspace(str[i])){
+			sexp* tmp;
+			tmp = to_sexp(str.substr(0, i));
+			tmp->next = parse_cons(str.substr(i + 1));
+			return tmp;
+		}
+		if(str[i] == '('){
+			sexp* tmp;
+			std::string t = pairUp(str.substr(i));
+			tmp = to_sexp(t);
+			std::cout<<tmp->toString()<<std::endl;
+			tmp->next = parse_cons(str.substr(i + t.length()));
+			return tmp;
+		}
+	}
+	//Default return
+	if(to_sexp(str) == SNIL){
+		return SNIL;
+	}
+	sexp* tmp;
+	tmp = to_sexp(str);
+	tmp->next = SNIL;
+	tmp->next->next = END;
+	return tmp;
 }
