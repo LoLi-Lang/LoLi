27b8044e33a5692b1a3403422b997cada922ea97
Z-Shang <shangzhanlin@gmail.com>
2014-07-29 14:23:08 +0800

    add if

--- src/LoLi2/loli_eval.cpp
+++ src/LoLi2/loli_eval.cpp
@@ -57,6 +57,66 @@ loliObj* c_eval(loliObj* exp){
 				}
 			}
 		case CONS:
+			if(head(exp)->equals(to_sym("if"))){
+				loliObj* cond = head(tail(exp));
+				if(nilp(tail(exp))){
+					loli_err("Need at least one expression for if");
+					return nil;
+				}
+				loliObj* wt = head(tail(tail(exp)));
+				loliObj* wf = head(tail(tail(tail(exp))));
+				switch(cond->type){
+					case CONS:
+						switch(head(cond)->type){
+							case PROC:
+								if(!head(cond)->PROC.rtype->equals(to_key("BOOL"))){
+									loli_err("Error! Need a BOOL for if, but get a " + toString(head(cond)->PROC.rtype));
+									return nil;
+								}else{
+									if(c_eval(cond)->equals(boolt)){
+										return c_eval(wt);
+									}else{
+										if(!nilp(wf) || wf != NULL){
+											return c_eval(wf);}
+										else{
+											return nil;
+										}
+									}
+								}
+							case LAMBDA:
+								if(!head(cond)->LAMBDA.rtype->equals(to_key("BOOL"))){
+									loli_err("Error! Need a BOOL for if, but get a " + toString(head(cond)->LAMBDA.rtype));
+									return nil;
+								}else{
+									if(c_eval(cond)->equals(boolt)){
+										return c_eval(wt);
+									}else{
+										if(!nilp(wf) || wf != NULL){
+											return c_eval(wf);}
+										else{
+											return nil;
+										}
+									}
+								}
+							default:
+								loli_err("Error! Need a BOOL for if, but get a " + toString(head(cond)->type));
+								return nil;
+						}
+					case BOOL:
+						if(cond->equals(boolt)){
+							return c_eval(wt);
+						}else{
+							if(!nilp(wf) || wf != NULL){
+								return c_eval(wf);}
+							else{
+								return nil;
+							}
+						}
+					default:
+						loli_err("Error! Need a BOOL for if, but get a " + toString(cond->type));
+						return nil;
+				}
+			}
 			return eval_list(exp);
 	}
 }
