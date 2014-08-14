e9dfd49267275c52969e1c35bdd533f0a21dfe3f
Z-Shang <shangzhanlin@gmail.com>
2014-08-14 17:35:36 +0800

    update

--- src/LoLi2/loli_repl.cpp
+++ src/LoLi2/loli_repl.cpp
@@ -20,13 +20,24 @@
 #include "include/loli_env.h"
 #include "include/loli_eval.h"
 #include "include/loli_reader.h"
+#include "include/loli_util.h"
+#include "include/loli_parser.h"
 
 #include <iostream>
 
 void c_repl(loliObj* env){
-	std::cout<<"LoLi > ";
-	std::string tmp = read_pair();
-	std::cout<<tmp<<std::endl;
+	while(true){
+		std::cout<<"LoLi > ";
+		std::string input = read_pair();
+		try{
+			//EVAL
+			loliObj* exp = parse_string(input, env);
+			std::cout<<toString(c_eval(exp))<<std::endl;
+		}catch(...){
+			loli_err("Error while evaluating: \n" + input);
+			break;
+		}
+	}
 }
 
 void c_repl(){
