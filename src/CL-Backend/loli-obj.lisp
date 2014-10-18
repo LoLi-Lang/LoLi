5503654cb54e94d3cbaddcbc5136274a0046f6b6
Z-Shang <shangzhanlin@gmail.com>
2014-10-18 17:25:40 -0500

    add typed "constructors"

--- src/CL-Backend/loli-obj.lisp
+++ src/CL-Backend/loli-obj.lisp
@@ -8,17 +8,89 @@
   (loli-type *type-obj* :type loli-type-class)
   env)
 
-(defconstant loli-nil
+(defun to-loli-sym (value env)
   (make-loli-obj
-   :value 'nil
+   :value value
    :loli-type *type-sym*
-   :env '()))
+   :env env))
+
+(defun to-loli-fn (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-fn*
+   :env env))
+
+(defun to-loli-obj (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-obj*
+   :env env))
+
+(defun to-loli-flt (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-flt*
+   :env env))
+
+(defun to-loli-int (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-int*
+   :env env))
+
+(defun to-loli-key (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-key*
+   :env env))
+
+(defun to-loli-num (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-num*
+   :env env))
+
+(defun to-loli-bool (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-bool*
+   :env env))
+
+(defun to-loli-char (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-char*
+   :env env))
+
+(defun to-loli-string (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-string*
+   :env env))
+
+(defun to-loli-cons (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-cons*
+   :env env))
+
+(defun to-loli-lambda (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-lambda*
+   :env env))
+
+(defun to-loli-proc (value env)
+  (make-loli-obj
+   :value value
+   :loli-type *type-proc*
+   :env env))
+
+(defconstant loli-nil
+  (to-loli-sym 'nil '()))
 
 (defconstant loli-t
-  (make-loli-obj
-   :value 't
-   :loli-type *type-sym*
-   :env '()))
+  (to-loli-sym 't '()))
 
 (defconstant loli-true
   (make-loli-obj
