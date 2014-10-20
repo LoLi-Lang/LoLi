c49e36d2823f53ec93034e0c7d15edfa67eb7063
Z-Shang <shangzhanlin@gmail.com>
2014-10-20 15:18:52 -0500

    add loli-cons loli-head loli-tail

--- src/CL-Backend/loli-parser.lisp
+++ src/CL-Backend/loli-parser.lisp
@@ -4,7 +4,6 @@
 (in-package #:loli)
 
 (defun tokenize (str)
-  str
   (loop for i = 0 then (1+ j)
      as j = (position #\Space str :start i)
      collect (subseq str i j)
@@ -14,7 +13,6 @@
   (let* ((trimed (string-trim '(#\Space #\Tab #\Newline) str))
          (token-lst (remove-if #'(lambda (x) (equalp x ""))
                                (tokenize trimed))))
-    token-lst
-    ))
+    token-lst))
 
 (provide 'loli-parser)
--- src/CL-Backend/loli-prim.lisp
+++ src/CL-Backend/loli-prim.lisp
@@ -1,2 +1,27 @@
 (require 'loli-package "package")
-(require 'loli-obj "loli-obj2")
+(require 'loli-obj "loli-obj")
+
+(in-package #:loli)
+
+(defstruct loli-cons-struct
+  (head loli-nil :type loli-obj)
+  (tail loli-nil :type loli-obj))
+
+(defun loli-cons (head &optional (tail loli-nil))
+  (loli-cons-struct :head head :tail tail))
+
+(defun loli-head (o)
+  (if (loli-obj-p o)
+      (if (loli-cons-struct-p (loli-obj-value o))
+          (loli-cons-struct-head (loli-obj-value o))
+          'ERR-NOT-LOLI-CONS)
+      'ERR-NOT-LOLI-OBJ))
+
+(defun loli-tail (o)
+  (if (loli-obj-p o)
+      (if (loli-cons-struct-p (loli-obj-value o))
+          (loli-cons-struct-tail (loli-obj-value o))
+          'ERR-NOT-LOLI-CONS)
+      'ERR-NOT-LOLI-OBJ))
+
+(provide 'loli-prim)
