03db502d9c6c6fdaadd715934c32998bd87e4dc7
Z-Shang <shangzhanlin@gmail.com>
2014-11-06 14:50:34 -0600

    tested loli-read-from-string) "

--- src/CL-Backend/loli-read.lisp
+++ src/CL-Backend/loli-read.lisp
@@ -2,20 +2,24 @@
 (require 'loli-obj "loli-obj")
 (require 'loli-repl "loli-repl")
 (require 'loli-type-class "loli-typeclass")
+(require 'loli-prim "loli-prim")
+(require 'loli-type-class "loli-typeclass")
 
 (defun loli-read-list (lst &optional (env '()))
-
-  )
+  (loli-cons (loli-read-single-obj (car lst) env)
+             (loli-read-single-obj (cdr lst) env)))
 
 (defun loli-read-single-obj (cl-obj &optional (env '()))
-  (typecase cl-obj
-    (integer (to-loli-int cl-obj env))
-    (float (to-loli-flt cl-obj env))
-    (string (to-loli-string cl-obj env))
-    (character (to-loli-char cl-obj env))
-    (keyword (to-loli-key cl-obj env))
-    (list (loli-read-list cl-obj env))
-    (symbol (to-loli-sym cl-obj env))))
+  (if (null cl-obj)
+      loli-nil
+      (typecase cl-obj
+        (integer (to-loli-int cl-obj env))
+        (float (to-loli-flt cl-obj env))
+        (string (to-loli-string cl-obj env))
+        (character (to-loli-char cl-obj env))
+        (keyword (to-loli-key cl-obj env))
+        (list (loli-read-list cl-obj env))
+        (symbol (to-loli-sym cl-obj env)))))
 
 (defun loli-read-from-string (str &optional (env '()))
   (loli-read-single-obj (read-from-string str) env))
