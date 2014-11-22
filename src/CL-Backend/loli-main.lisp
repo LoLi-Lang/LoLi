48797dcd8c1335158e055b38b2477946716fdf9f
Z-Shang <shangzhanlin@gmail.com>
2014-11-22 16:55:51 -0600

    remove the start-up warnings, add start information

--- src/CL-Backend/loli-main.lisp
+++ src/CL-Backend/loli-main.lisp
@@ -1,5 +1,9 @@
-(require 'loli-package "package")
-(require 'loli-repl "loli-repl")
+(let* ((*standard-output* (make-broadcast-stream))
+       (*error-output* *standard-output*))
+  (require 'loli-package "package")
+  (require 'loli-repl "loli-repl"))
+
+(format *standard-output* "~%LoLi PRPR!~%LoLi is a Free Software and you can do whatever you want with it under the licence GPLv3 or any later version. ~%")
 
 (in-package #:loli)
 
